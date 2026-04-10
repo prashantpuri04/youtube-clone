bd2b37dfc8b7e2bd0e90b3a47a4e3277e4ab76ba
prashantpuri <prashantpuri04@gmail.com>
2026-04-10 16:34:53 +0530

    Initial-youtube-structure

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import Head from './components/Head';
+import Body from './components/Body';
+import Sidebar from './components/Sidebar';
+
+/**
+ * 
+ * @returns Head 
+ * body
+ * Sidebar
+ *  MenuItems
+ * Main container
+ *  ButtonList
+ * VideoContainer
+ *  VideoCard
+ *  VideoDetails
+ */
+function App() {
+  
+  return (
+    <>
+    <div>
+      <Head />
+      <Body />
+      
+    </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ButtonList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ButtonList(){
+    return (
+        <div>
+            <h1 className='text-3xl font-bold'>Button List</h1>
+        </div>
+    )
+}
